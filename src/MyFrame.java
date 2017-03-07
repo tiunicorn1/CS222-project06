916dc1e97b138519357bfc078e278f69a77d88e4
Ray Harris <ray.harris@mcckc.edu>
2017-03-06 23:40:35 -0600

    Added all files

---
+++ src/MyFrame.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by E0660645 on 3/6/2017.
+ */
+public class MyFrame extends JFrame
+{
+    private int count;
+    private double radius;
+    private double totalPerimeter;
+    private double totalArea;
+    private double averagePerimeter;
+    private double averageArea;
+
+    public MyFrame()
+    {
+        //  prompts and calculations
+    }
+
+    @Override
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        g.drawString(      );
+    }
+}
